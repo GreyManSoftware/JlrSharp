9c7e96c458a35b9f4eeb26bb7a73da007644a536
Chris Davies <89serenity@gmail.com>
2019-12-28 17:40:43 +0000

    Initial commits

---
+++ JlrSharp/JlrSharpConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace GreyMan.JlrSharp
+{
+    /// <summary>
+    /// The interface for the JLR InControl connectivity
+    /// </summary>
+    public class JlrSharpConnection
+    {
+        private Uri IfasBaseUrl = new Uri("https://ifas.prod-row.jlrmotor.com/ifas/jlr"); // Used for generating tokens
+        private Uri IfopBaseUrl = new Uri("https://ifop.prod-row.jlrmotor.com/ifop/jlr"); // Used for registering devices
+        private Uri If9BaseUrl = new Uri("https://if9.prod-row.jlrmotor.com/if9/jlr"); // Used for vehicle requests
+
+        private string _username;
+        private string _password;
+        private string _pin;
+        private string _deviceId;
+        private string _refreshToken;
+
+        public JlrSharpConnection(string username, string password)
+        {
+            _username = username;
+            _password = Convert.ToBase64String(Encoding.ASCII.GetBytes(password));
+        }
+
+        public void Connect()
+        {
+
+        }
+    }
+}
---
+++ JlrSharpRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JlrSharpRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
